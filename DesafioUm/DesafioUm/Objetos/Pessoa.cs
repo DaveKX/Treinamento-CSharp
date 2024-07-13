423c964c73a9bc2dbe205907425c53633f6008b1
Dave <90359076+DaveKX@users.noreply.github.com>
2024-07-13 12:13:22 +0100

    add

---
+++ DesafioUm/DesafioUm/Objetos/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioUm.Objetos
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage = "O Nome é obrigatório")]
+        [StringLength(100, ErrorMessage = "O Nome pode ter no máximo 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O Telefone é obrigatório")]
+        [RegularExpression(@"^\(\d{2}\)\d{5}-\d{4}$", ErrorMessage = "O Telefone deve estar no formato (xx)xxxxx-xxxx")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O Endereço é obrigatório")]
+        [StringLength(200, ErrorMessage = "O Endereço pode ter no máximo 200 caracteres")]
+        public string Endereço { get; set; }
+
+        //public Pessoa Cadastrar(string _nome, string _endereco, string _telefone)
+        //{
+        //    Nome = _nome;
+        //    Endereço = _endereco;
+        //    Telefone = PadronizarTelefone(_telefone);
+        //    return this;
+        //}
+
+        override public string ToString()
+        {
+            return "[Nome: " + Nome + "; Endereço: " + Endereço + "; Telefone: " + Telefone + "]";
+        }
+
+        //private string PadronizarTelefone(string telefone)
+        //{
+        //    string telFinal = "(" + telefone.Substring(0, 2) + ")" + telefone.Substring(2, 5) + "-" + telefone.Substring(7);
+        //    return telFinal;
+        //}
+    }
+}
